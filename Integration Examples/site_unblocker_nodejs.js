27f86ffb62f1f8094a590be00dc3ca9125d6030b
Žilvinas <159907476+sakanamk@users.noreply.github.com>
2025-04-25 14:48:16 +0300

    Update site_unblocker_nodejs.js

--- Integration Examples/site_unblocker_nodejs.js
+++ Integration Examples/site_unblocker_nodejs.js
@@ -5,12 +5,12 @@ const username = 'USERNAME';
 const password = 'PASSWORD';
 
 const agent = createHttpsProxyAgent(
-  `http://${username}:${password}@unblock.smartproxy.com:60000`
+  `http://${username}:${password}@unblock.decodo.com:60000`
 );
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-const response = await fetch('https://ip.smartproxy.com/', {
+const response = await fetch('https://ip.decodo.com/', {
   method: 'get',
   agent: agent
 });
